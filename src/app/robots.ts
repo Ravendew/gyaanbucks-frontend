3ac6b855a57d11b135dc35e3c708467dc73b6b3d
SaiPrashanthP <digitalrevoltindia@gmail.com>
2026-05-05 14:24:02 +0530

    Fix robots for Facebook bot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,21 @@ export default function robots(): MetadataRoute.Robots {
   const siteUrl = 'https://gyaanbucks.com';
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/admin/', '/auth/', '/profile/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/', '/auth/', '/profile/'],
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: 'Facebot',
+        allow: '/',
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
     host: siteUrl,
   };
